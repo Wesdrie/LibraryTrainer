84e508ec9b359530fa249566ae4628f1892cf558
Wesdrie <hen3wes@gmail.com>
2022-10-24 21:02:22 +0200

    Wiht Index Numebrs

--- CallAreas.cs
+++ CallAreas.cs
@@ -8,17 +8,20 @@ namespace LibraryTrainer
 {
     internal class CallAreas
     {
+        int areaIndex;
         string areaNumber, areaName, areaDescription;
 
-        public CallAreas(string areaNumber, string areaName, string areaDescription)
+        public CallAreas(int areaIndex, string areaNumber, string areaName, string areaDescription)
         {
+            this.areaIndex = areaIndex;
             this.areaNumber = areaNumber;
             this.areaName = areaName;
             this.areaDescription = areaDescription;
         }
 
+        public int AreaIndex { get; set; }
         public string AreaNumber { get; set; }
         public string AreaName { get; set; }
-        public int AreaDescription { get; set; }
+        public string AreaDescription { get; set; }
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -14,25 +14,25 @@ namespace LibraryTrainer
     public partial class WindowAreas : Form
     {
         /// <summary>
-        /// VARIBLES
+        /// VARIBLES & AREAS OBJECT WITH VALUES (EVENTUALLY MOVE TO DB)
         /// </summary>
         Tools wrench = new Tools();
 
         CallAreas[] callAreas =
         {
-            new CallAreas("000 - 099", "General Works", "Basic Information, Encyclopedias & Record Books"),
-            new CallAreas("100 - 199", "Philosophy & Psychology", "Paranormal Phenomena, Ethics & “Who Am I”"),
-            new CallAreas("200 - 299", "Religion", "Christians, Hindus & Mythology"),
-            new CallAreas("300 - 399", "Social Sciences", "Laws, Cultures & Folklore"),
-            new CallAreas("400 - 499", "Languages", "Dictionaries, Speech & Sign Language"),
-            new CallAreas("500 - 599", "Science", "Physics, Chemistry & Biology"),
-            new CallAreas("600 - 699", "Technology", "Inventions, Robots & Transportation"),
-            new CallAreas("700 - 799", "The Arts", "Comics, Music & Sports"),
-            new CallAreas("800 - 899", "Literature & Rhetoric", "Shakespeare, Classic Literature & Riddles"),
-            new CallAreas("900 - 999", "History & Geography", "Exploration, Countries & Factual Events")
+            new CallAreas(0, "000 - 099", "General Works", "Basic Information, Encyclopedias & Record Books"),
+            new CallAreas(1, "100 - 199", "Philosophy & Psychology", "Paranormal Phenomena, Ethics & “Who Am I”"),
+            new CallAreas(2, "200 - 299", "Religion", "Christians, Hindus & Mythology"),
+            new CallAreas(3, "300 - 399", "Social Sciences", "Laws, Cultures & Folklore"),
+            new CallAreas(4, "400 - 499", "Languages", "Dictionaries, Speech & Sign Language"),
+            new CallAreas(5, "500 - 599", "Science", "Physics, Chemistry & Biology"),
+            new CallAreas(6, "600 - 699", "Technology", "Inventions, Robots & Transportation"),
+            new CallAreas(7, "700 - 799", "The Arts", "Comics, Music & Sports"),
+            new CallAreas(8, "800 - 899", "Literature & Rhetoric", "Shakespeare, Classic Literature & Riddles"),
+            new CallAreas(9, "900 - 999", "History & Geography", "Exploration, Countries & Factual Events")
         };
 
-        Dictionary<string, object> areaDictionary = new Dictionary<string, object>();
+        Dictionary<int, CallAreas> areaDictionary = new Dictionary<int, CallAreas>();
 
         //Dictionary<string, string> dictionaryAreas = new Dictionary<string, string>()
         //{
@@ -78,12 +78,10 @@ namespace LibraryTrainer
                 /// CONNECT TO DATABASE TO PULL SCORES
                 /// </remarks>
                 
-                /// <remarks>
-                /// IMPLEMENTS DICITONARY
-                /// </remarks>
                 foreach(var area in callAreas)
                 {
-                    areaDictionary.Add(area.AreaNumber, area);
+                    areaDictionary.Add(area.AreaIndex, area);
+                    Console.WriteLine(area);
                 };
                 
                 TimerAreas.Start();
