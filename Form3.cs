2c94cb5e55694d1467be553c67c7ee0621eb1308
Wesdrie <hen3wes@gmail.com>
2022-10-24 21:32:09 +0200

    What A Mess Update.

--- Form3.cs
+++ Form3.cs
@@ -18,48 +18,25 @@ namespace LibraryTrainer
         /// </summary>
         Tools wrench = new Tools();
 
-        CallAreas[] callAreas =
-        {
-            new CallAreas(0, "000 - 099", "General Works", "Basic Information, Encyclopedias & Record Books"),
-            new CallAreas(1, "100 - 199", "Philosophy & Psychology", "Paranormal Phenomena, Ethics & “Who Am I”"),
-            new CallAreas(2, "200 - 299", "Religion", "Christians, Hindus & Mythology"),
-            new CallAreas(3, "300 - 399", "Social Sciences", "Laws, Cultures & Folklore"),
-            new CallAreas(4, "400 - 499", "Languages", "Dictionaries, Speech & Sign Language"),
-            new CallAreas(5, "500 - 599", "Science", "Physics, Chemistry & Biology"),
-            new CallAreas(6, "600 - 699", "Technology", "Inventions, Robots & Transportation"),
-            new CallAreas(7, "700 - 799", "The Arts", "Comics, Music & Sports"),
-            new CallAreas(8, "800 - 899", "Literature & Rhetoric", "Shakespeare, Classic Literature & Riddles"),
-            new CallAreas(9, "900 - 999", "History & Geography", "Exploration, Countries & Factual Events")
-        };
-
-        Dictionary<int, CallAreas> areaDictionary = new Dictionary<int, CallAreas>();
-
-        //Dictionary<string, string> dictionaryAreas = new Dictionary<string, string>()
-        //{
-        //    {"000 - 099", "Basic Information, Encyclopedias & Record Books"},
-        //    {"100 - 199", "Paranormal Phenomena, Ethics & “Who Am I”"},
-        //    {"200 - 299", "Christians, Hindus & Mythology"},
-        //    {"300 - 399", "Laws, Cultures & Folklore"},
-        //    {"400 - 499", "Dictionaries, Speech & Sign Language"},
-        //    {"500 - 599", "Physics, Chemistry & Biology"},
-        //    {"600 - 699", "Inventions, Robots & Transportation"},
-        //    {"700 - 799", "Comics, Music & Sports"},
-        //    {"800 - 899", "Shakespeare, Classic Literature & Riddles"},
-        //    {"900 - 999", "Exploration, Countries & Factual Events"}
+        /// <summary>
+        /// STORING OBJECT IN DICTIONARY RETURNS NULL VALUES ?
+        /// </summary>
+        //Dictionary<int, CallAreas> areaDictionary = new Dictionary<int, CallAreas>();
+        //List<CallAreas> areaList = new List<CallAreas>() {
+        //    new CallAreas(0, "000 - 099", "General Works", "Basic Information, Encyclopedias & Record Books"),
+        //    new CallAreas(1, "100 - 199", "Philosophy & Psychology", "Paranormal Phenomena, Ethics & “Who Am I”"),
+        //    new CallAreas(2, "200 - 299", "Religion", "Christians, Hindus & Mythology"),
+        //    new CallAreas(3, "300 - 399", "Social Sciences", "Laws, Cultures & Folklore"),
+        //    new CallAreas(4, "400 - 499", "Languages", "Dictionaries, Speech & Sign Language"),
+        //    new CallAreas(5, "500 - 599", "Science", "Physics, Chemistry & Biology"),
+        //    new CallAreas(6, "600 - 699", "Technology", "Inventions, Robots & Transportation"),
+        //    new CallAreas(7, "700 - 799", "The Arts", "Comics, Music & Sports"),
+        //    new CallAreas(8, "800 - 899", "Literature & Rhetoric", "Shakespeare, Classic Literature & Riddles"),
+        //    new CallAreas(9, "900 - 999", "History & Geography", "Exploration, Countries & Factual Events"),
         //};
-        //Dictionary<string, string> dictionaryNumbers = new Dictionary<string, string>()
-        //{
-        //    {"000 - 099", "General Works"},
-        //    {"100 - 199", "Philosophy & Psychology"},
-        //    {"200 - 299", "Religion"},
-        //    {"300 - 399", "Social Sciences"},
-        //    {"400 - 499", "Languages"},
-        //    {"500 - 599", "Science"},
-        //    {"600 - 699", "Technology"},
-        //    {"700 - 799", "The Arts"},
-        //    {"800 - 899", "Literature & Rhetoric"},
-        //    {"900 - 999", "History & Geography"}
-        //};
+
+        Dictionary<string, string> dictionaryAreas = new Dictionary<string, string>();
+        Dictionary<string, string> dictionaryText = new Dictionary<string, string>();
 
         int timerTicker, userScore;
         public WindowAreas()
@@ -74,16 +51,33 @@ namespace LibraryTrainer
         {
             try
             {
-                /// <remarks>
-                /// CONNECT TO DATABASE TO PULL SCORES
-                /// </remarks>
-                
-                foreach(var area in callAreas)
+                dictionaryAreas.Add("000 - 099", "General Works");
+                dictionaryAreas.Add("100 - 199", "Philosophy & Psychology");
+                dictionaryAreas.Add("200 - 299", "Religion");
+                dictionaryAreas.Add("300 - 399", "Social Sciences");
+                dictionaryAreas.Add("400 - 499", "Languages");
+                dictionaryAreas.Add("500 - 599", "Science");
+                dictionaryAreas.Add("600 - 699", "Technology");
+                dictionaryAreas.Add("700 - 799", "The Arts");
+                dictionaryAreas.Add("800 - 899", "Literature & Rhetoric");
+                dictionaryAreas.Add("900 - 999", "History & Geography");
+
+                dictionaryText.Add("000 - 099", "General Works");
+                dictionaryText.Add("100 - 199", "Philosophy & Psychology");
+                dictionaryText.Add("200 - 299", "Religion");
+                dictionaryText.Add("300 - 399", "Social Sciences");
+                dictionaryText.Add("400 - 499", "Languages");
+                dictionaryText.Add("500 - 599", "Science");
+                dictionaryText.Add("600 - 699", "Technology");
+                dictionaryText.Add("700 - 799", "The Arts");
+                dictionaryText.Add("800 - 899", "Literature & Rhetoric");
+                dictionaryText.Add("900 - 999", "History & Geography");
+
+                foreach (string key in dictionaryAreas.Keys)
                 {
-                    areaDictionary.Add(area.AreaIndex, area);
-                    Console.WriteLine(area);
-                };
-                
+                    Console.WriteLine(key);
+                }
+
                 TimerAreas.Start();
             }
             catch (Exception ex)
